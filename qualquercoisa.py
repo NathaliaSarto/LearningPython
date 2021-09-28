e3435f509704edbf8a2169292ceac1fca73bfa40
Nath <nathaliasarto3@gmail.com>
2021-09-27 22:23:57 -0300

    primeiro código

---
+++ qualquercoisa.py
@@ -0,0 +1,9 @@
+character_name = "Waliksinho"
+character_age = 52
+print("There once was a man named " + character_name + ",")  
+print("he was " + str(character_age) + " years old. ")  
+
+character_name = "Waliks"
+print("He really liked the name " + character_name + ",")
+print("but didn't like being " + str(character_age) +".")
+print(f"asdfghyh {character_age} sdertgyhybb ")   
